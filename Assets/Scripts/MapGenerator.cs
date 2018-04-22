01cb61330ad7f8f00dbe4b19f742433f9f476a65
ThomasEnniss <teenniss@utas.edu.au>
2018-04-22 21:02:51 +1000

    Map Grid
    I thought the map grid would be a good idea so I tried it.
    Going to try a different Graph method.

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -5,13 +5,13 @@ using System.Collections.Generic;
 public class MapGenerator : MonoBehaviour {
 
 	public GameObject player;
-	public GameObject parent_map;
+
 	public GameObject wall_prefab;
 	public GameObject floor_prefab;
 	public GameObject chest;
 
-	public int map_width_x;
-	public int map_length_z;
+	public int room_space_width_x;
+	public int room_space_length_z;
 	public int number_of_rooms;
 
 	public int min_room_width;
@@ -22,17 +22,21 @@ public class MapGenerator : MonoBehaviour {
 
 	public int max_chest_count;
 
-	int current_chest_count;
+	int[,] room_grid;
 
-	int[,] map_structure;
+	int grid_x_constant;
+	int grid_z_constant;
 
-	bool player_in_map;
+	List<Room> rooms_to_plot;
 
-	// Update is called once per frame
+
+
 	void Start () {
-		player_in_map = false;
-		map_structure = new int[map_width_x,map_length_z];
-		current_chest_count = 0;
+		grid_x_constant = min_room_width+2;
+		grid_z_constant = room_space_length_z+2;
+		//player_in_map = false;
+		room_grid = new int[room_space_width_x,room_space_length_z];
+		rooms_to_plot = new List<Room> ();
 		GenerateMap ();
 		DrawMap ();
 	}
@@ -44,97 +48,83 @@ public class MapGenerator : MonoBehaviour {
 
 	void FillMap(){
 
-		for(int lengthZ = 0;lengthZ<map_length_z;lengthZ++)
+		for(int lengthZ = 0;lengthZ<room_space_length_z;lengthZ++)
 		{
-			for(int widthX = 0;widthX<map_width_x;widthX++)
+			for(int widthX = 0;widthX<room_space_width_x;widthX++)
 			{
-				map_structure[widthX,lengthZ] = 1;
+				room_grid[widthX,lengthZ] = 0;
 			}
 		}
 	}
 
 	void CreateRooms(){
 
-
-
 		/*These change based on the current rooms position and the roomnode.*/
-		int initial_x_min_constraint = 1;
-		int initial_x_max_constraint = map_width_x- 1- max_room_width;
-		int initial_z_min_constraint = 1;
-		int initial_z_max_constraint = map_length_z - 1 - max_room_length;			
+		int initial_x_min_constraint = 0;
+		int initial_x_max_constraint = room_space_width_x - 1;
+		int initial_z_min_constraint = 0;
+		int initial_z_max_constraint = room_space_length_z - 1;			
 
 
 		for (int i = 0; i < number_of_rooms; i++) {
 
-			int top_x = (int)Random.Range (initial_x_min_constraint,initial_x_max_constraint);
-			int top_z = (int)Random.Range (initial_z_min_constraint,initial_z_max_constraint);
-			int width = (int)Random.Range (min_room_width,max_room_width);
-			int length = (int)Random.Range (min_room_length,max_room_length);
+			int grid_x = (int)Random.Range (initial_x_min_constraint,initial_x_max_constraint);
+			int grid_z = (int)Random.Range (initial_z_min_constraint,initial_z_max_constraint);
 
-			Room new_room = new Room (top_x,top_z,width,length);
+			if (room_grid[grid_x,grid_z] == 0) {
 
-			if ((int)Random.Range (0, 5) == 0 && current_chest_count < max_chest_count) {
-				print ("Adding Chest to room " + i);
-				new_room.AddChest ();
+				int width = (int)Random.Range (min_room_width,max_room_width);
+				int length = (int)Random.Range (min_room_length,max_room_length);
+
+				int top_x = (int)Random.Range (grid_x*grid_x_constant,grid_x*grid_x_constant+grid_x_constant-max_room_width-1);
+				int top_z = (int)Random.Range (grid_z*grid_z_constant,grid_z*grid_z_constant+grid_z_constant-max_room_length-1);
+
+				Room new_room = new Room (top_x,top_z,width,length);
+				rooms_to_plot.Add (new_room);
+				room_grid [grid_x, grid_z] = 1;
+
+			} else {
+				i++;
 			}
-
-			PlotRoom (new_room);
 		}
 	}
 
-	/*Fills the room area on the map. Marks it as 0*/
-	void PlotRoom(Room room){
+	/*Fills the room area on the map.*/
+	/*void PlotRoom(Room room){
 
 		for(int lengthZ = room.top_z; lengthZ < room.top_z + room.length_z; lengthZ++)
 		{
 			for(int widthX = room.top_x; widthX < room.top_x + room.width_x; widthX++)
 			{
 				if (lengthZ == room.top_z || lengthZ == room.top_z + room.length_z - 1 || widthX == room.top_x || widthX == room.top_x + room.width_x - 1) {
-					if (map_structure [widthX, lengthZ] != 0) {
-						map_structure [widthX, lengthZ] = 2;
+					if (room_grid [widthX, lengthZ] != 0) {
+						room_grid [widthX, lengthZ] = 2;
 					}
 				} else {
 					
-						map_structure [widthX, lengthZ] = 0;					
+					room_grid [widthX, lengthZ] = 0;					
 				}
 			}
 		}
-
-		if (room.contents.Count > 0) {
-
-			foreach (Vector3 chest_position in room.contents) {
-				GameObject new_chest = (GameObject)Instantiate (chest, chest_position, Quaternion.identity);
-				new_chest.transform.SetParent (parent_map.transform);
-			}
-		}
-	}
+	}*/
 
 	void DrawMap(){
 
-		for(int lengthZ = 0;lengthZ<map_length_z;lengthZ++)
-		{
-			for(int widthX = 0;widthX<map_width_x;widthX++)
+		foreach (Room new_room in rooms_to_plot) {
+
+			for(int lengthZ = new_room.top_z;lengthZ<new_room.top_z+new_room.length_z;lengthZ++)
 			{
-				
-				if(map_structure[widthX,lengthZ] == 0){
-
-					if ((int)Random.Range (0, 10) == 0 && !player_in_map) {
-						player_in_map = true;
-						GameObject new_player = (GameObject)Instantiate (player, new Vector3 (widthX,1.0f,lengthZ), Quaternion.identity);
-						new_player.transform.SetParent (parent_map.transform);
+				for(int widthX = new_room.top_x;widthX<new_room.top_x+new_room.width_x;widthX++)
+				{
+					/*Instantiate a wall and floor tile.*/
+					if (lengthZ == new_room.top_z || lengthZ == new_room.top_z + new_room.length_z - 1 || widthX == new_room.top_x || widthX == new_room.top_x + new_room.width_x - 1) {
+						GameObject wall_tile = (GameObject)Instantiate (wall_prefab, new Vector3 (widthX,0.75f,lengthZ), Quaternion.identity);
+						GameObject floor_tile = (GameObject)Instantiate (floor_prefab, new Vector3 (widthX,0.0f,lengthZ), Quaternion.identity);
+					} else {
+						GameObject floor_tile = (GameObject)Instantiate (floor_prefab, new Vector3 (widthX,0.0f,lengthZ), Quaternion.identity);				
 					}
-
-					GameObject floor_tile = (GameObject)Instantiate (floor_prefab, new Vector3 (widthX,0.0f,lengthZ), Quaternion.identity);
-					floor_tile.transform.SetParent (parent_map.transform);
-				}
-
-				if(map_structure[widthX,lengthZ] == 2){
-					GameObject wall_tile = (GameObject)Instantiate (wall_prefab, new Vector3 (widthX,0.75f,lengthZ), Quaternion.identity);
-					wall_tile.transform.SetParent (parent_map.transform);
-					GameObject floor_tile = (GameObject)Instantiate (floor_prefab, new Vector3 (widthX,0.0f,lengthZ), Quaternion.identity);
-					floor_tile.transform.SetParent (parent_map.transform);
 				}
 			}
-		}
+		}		
 	}
 }
